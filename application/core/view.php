21de048380a3058f90effaddbd02f8d6dbad886f
meekot <github@meekot.com>
2019-09-29 14:36:24 +0200

    change path in generate view

--- application/core/view.php
+++ application/core/view.php
@@ -5,7 +5,9 @@ class View
 
 
     function generate($content_view, $template_view, $pageData = null)
-    {
+    {   $content_view = VIEW_PATH.$content_view;
+        $template_view = VIEW_PATH.$template_view;
+
         include VIEW_PATH.$template_view;
     }
 
